4e43ca6858e584beaf0d231ae0f692c10174632d
SAK74 <sergijks@gmail.com>
2023-07-31 22:48:07 +0200

    feat: read api from file in prod mode

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,39 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { SetHeaderInterceptor } from './interceptors/setHeader.interceptor';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { writeFile } from 'fs';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync, writeFile } from 'fs';
 import { resolve } from 'path';
-import { stringify } from 'yaml';
+import { parse, stringify } from 'yaml';
 
 const PORT = process.env.PORT;
+const apiFileURL = resolve(__dirname, '../doc/open-api.yaml');
 
 const config = new DocumentBuilder()
   .setTitle('Home Library Service')
   .setDescription('Home music library service')
   .setVersion('1.0.0')
-  .addServer('/api')
   .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const apiDocument = SwaggerModule.createDocument(app, config);
+  let apiDocument: OpenAPIObject;
   if (process.env.NODE_ENV === 'development') {
+    apiDocument = SwaggerModule.createDocument(app, config);
     writeDocAPI(stringify(apiDocument));
+  } else {
+    const data = readFileSync(apiFileURL, { encoding: 'utf8' });
+    apiDocument = parse(data);
   }
-  SwaggerModule.setup('api', app, apiDocument);
+  SwaggerModule.setup('docs', app, apiDocument);
+  console.log('\x1b[95mDocs have building in /docs path\x1b[0m');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new SetHeaderInterceptor());
   await app.listen(PORT);
 }
 bootstrap();
 const writeDocAPI = (document: string) => {
-  const fileURL = resolve(__dirname, '../doc/open-api.yaml');
-  writeFile(fileURL, document, { encoding: 'utf8' }, () => {
-    console.log(`\x1b[95mAPI docs has been saved to ${fileURL}\x1b[0m`);
+  writeFile(apiFileURL, document, { encoding: 'utf8' }, () => {
+    console.log(`\x1b[95mAPI docs has been saved to ${apiFileURL}\x1b[0m`);
   });
 };
